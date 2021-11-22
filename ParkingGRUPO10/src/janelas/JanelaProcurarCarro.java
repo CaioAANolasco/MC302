c7498c9ac7a8cfd0af5622b47a39ff828ad074f8
Caio Nolasco <32886347+CaioAANolasco@users.noreply.github.com>
2021-11-22 14:58:12 -0300

    Add files via upload

---
+++ ParkingGRUPO10/src/janelas/JanelaProcurarCarro.java
@@ -0,0 +1,84 @@
+package janelas;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+import locais.Estacionamento;
+import usuarios.ClienteGaragem;
+
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class JanelaProcurarCarro { //inicio da JanelaProcurarCarro
+
+	private JFrame frmProcurar;
+	private JTextField textPlaca;
+	private JButton btnCancelar;
+
+	public static void dadosProcurar(Estacionamento estacionamento) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JanelaProcurarCarro window = new JanelaProcurarCarro(estacionamento);
+					window.frmProcurar.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public JanelaProcurarCarro(Estacionamento estacionamento) { //construtor de JanelaProcurarCarro
+		initialize(estacionamento);
+	}
+
+	private void initialize(Estacionamento estacionamento) {
+		frmProcurar = new JFrame();
+		frmProcurar.setTitle("Procurar");
+		frmProcurar.setBounds(100, 100, 288, 437);
+		frmProcurar.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmProcurar.getContentPane().setLayout(null);
+		
+		textPlaca = new JTextField();
+		textPlaca.setBounds(77, 75, 116, 22);
+		frmProcurar.getContentPane().add(textPlaca);
+		textPlaca.setColumns(10); //campo para leitura da placa do carro do cliente
+		
+		JLabel lblPlaca = new JLabel("Placa:");
+		lblPlaca.setBounds(75, 46, 56, 16);
+		frmProcurar.getContentPane().add(lblPlaca);
+		
+		JButton btnProcurar = new JButton("Procurar");
+		btnProcurar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String placa = textPlaca.getText(), saida = ""; char linha;
+				
+				ClienteGaragem clienteProcurado = estacionamento.buscaClienteGaragem(placa);  //procura o cliente pelo metodo de Estacionamento
+				linha = (char)(clienteProcurado.getCarro().getLocalizacao()[0] + 65);
+				saida = saida + "O carro esta estacionado no andar " + clienteProcurado.getCarro().getAndarLocalizado() + ", linha " +
+				linha + ", coluna " + clienteProcurado.getCarro().getLocalizacao()[1];
+				JOptionPane.showMessageDialog(frmProcurar, saida);
+				frmProcurar.dispose();
+				
+			}
+		});
+		btnProcurar.setBounds(31, 158, 207, 87);
+		frmProcurar.getContentPane().add(btnProcurar);
+		
+		btnCancelar = new JButton("Cancelar");
+		btnCancelar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				frmProcurar.dispose();
+			}
+		});
+		btnCancelar.setBounds(31, 268, 207, 87);
+		frmProcurar.getContentPane().add(btnCancelar); //botao para cancelar operacao
+	}
+
+} //fim da classe JanelaProcurarCarro
